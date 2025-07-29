80b7185703fbe119942154fcf4d74fa11233a441
guiyeIbarra <guiyeibarra@hotmail.com>
2025-07-29 11:25:48 -0300

    cambio a railway

--- db.js
+++ db.js
@@ -1,13 +1,24 @@
 // db.js
 const mysql = require('mysql2');
 
+
+/*
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',           // o el usuario que uses
   password: '',           // o la contraseña que configuraste
   database: 'negocio'     // la que creaste en phpMyAdmin
 });
+*/
 
+const connection = mysql.createConnection({
+  host: 'hopper.proxy.rlwy.net',
+  port: 41480,
+  user: 'root',
+  password: 'UiflRBPDQpmYBukgxUabZiszvqVNnPLC',
+  database: 'railway'
+});
+/*
 db.connect(err => {
   if (err) {
     console.error('❌ Error al conectar a MySQL:', err);
@@ -17,3 +28,14 @@ db.connect(err => {
 });
 
 module.exports = db;
+*/
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    return;
+  }
+  console.log('Conexión a la base de datos remota exitosa.');
+});
+
+module.exports = connection;
